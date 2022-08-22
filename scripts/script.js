81f4aa502a6a9bcdbbe7437766081325b8ef9566
Laura B <lmcarcione@gmail.com>
2022-08-22 00:32:41 -0700

    WIP Drag and drop

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,22 @@
 
 // Misc variables - page elements etc.
 	const audioPlayer = document.getElementById("audioplayer");
-	const audioFileLink = document.getElementById("audiofile")
+	const audioFileLink = document.getElementById("audiofile");
+	const dropZone = document.getElementById("drop_zone");
 
 // Button handlers
 	const fileRequestButton = document.getElementById("filereq");
 
-	fileRequestButton.addEventListener("click", function(event){
+//Enable drops
+	dropZone.addEventListener("dragover", function(e) {
+		dragoverHandler(e);
+	});
+
+	dropZone.addEventListener("drop", function(e) {
+		dropHandler(e);
+	});
+
+	fileRequestButton.addEventListener("click", function(e){
 		read1();
 	});
 
@@ -18,7 +28,26 @@
 		var audioname = audioFileLink.value;
 		audioPlayer.setAttribute("src",audioname);
 
-		alert(`You are attempting to load ${audioname}`);
+		console.log(`You are attempting to load ${audioname}`);
+	}
+
+	function dragoverHandler(e){
+		e.preventDefault();
+		console.log("dragover event");
+	}
+
+	function dropHandler(e){
+		e.stopPropagation();
+		e.preventDefault();
+		var data = e.dataTransfer;
+		var lf = data.files.length;
+		console.log(`You are dropping ${lf} files`);
+		if (lf > 0) {
+			var file = data.files[0];
+			var audioname = file.name.substring(0,file.name.length-4);
+			console.log(`${audioname}`)
+			audioPlayer.setAttribute("src",URL.createObjectURL(file));
+		}
 	}
 
 	function playAudio() {
@@ -28,4 +57,7 @@
 
 	}
 
+
 })();
+
+	
